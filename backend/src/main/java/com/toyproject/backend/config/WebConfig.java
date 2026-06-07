0aff45bd55114c03c8285f9ac0080fee54d515ef
이광현 <rhkd4129@naver.com>
2026-06-07 18:05:48 +0900

    둘다

--- backend/src/main/java/com/toyproject/backend/config/WebConfig.java
+++ backend/src/main/java/com/toyproject/backend/config/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "http://43.200.234.146")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")//보낼수있는 헤더
                 .exposedHeaders("Authorization", "Content-Disposition")  //받을수잇는헤더
